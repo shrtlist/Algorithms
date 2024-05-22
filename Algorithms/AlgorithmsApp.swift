ba6353a08a7c843bf614ee6b17960f2ad2c0995c
Marco Abundo <shrtlist@gmail.com>
2024-05-22 14:55:20 -0700

    Initial Commit

---
+++ Algorithms/AlgorithmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlgorithmsApp.swift
+//  Algorithms
+//
+//  Created by Marco Abundo on 5/22/24.
+//
+
+import SwiftUI
+
+@main
+struct AlgorithmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Algorithms/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Algorithms
+//
+//  Created by Marco Abundo on 5/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
